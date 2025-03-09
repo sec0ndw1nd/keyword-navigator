e69e348c6791bb5e1b5d92a325b72bb49ad42a55
sec0ndw1nd <phj98521@naver.com>
2025-03-10 01:47:19 +0900

    chore: delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.commands.onCommand.addListener((command) => {
-  chrome.storage.sync.get("keywords", (data) => {
-    const entry = (data.keywords || []).find((k) => k.keyword === command);
-    if (entry) {
-      chrome.tabs.create({ url: entry.url });
-    }
-  });
-});
